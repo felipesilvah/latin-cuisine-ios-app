fc4385901ef150509fef82a317d73b162d70284f
felipesilvah <felipesilvah@gmail.com>
2017-02-04 10:01:06 -0500

    Agregar archivos generados

---
+++ config.js
@@ -0,0 +1,21 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'latin-cuisine-ios',
+		wp_ws_url : 'http://latincuisine.ca/wp-appkit-api/latin-cuisine-ios',
+		wp_url : 'http://latincuisine.ca',
+		theme : 'q-ios',
+		version : '1.0.0',
+		app_title : 'Latin Cuisine IOS',
+		app_platform : 'ios',
+		gmt_offset : 0,
+		debug_mode : 'off',
+		auth_key : 'nWg4rcseLL6a6AFps6dkRrMXs4SnxT7FSh1VWAayUrNKdvDNgxYEo6oq5kgs2E7G',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
